b69d2f3540a6217be64b83ed8a9eeb5173a4ac48
sai kiran pathivada <saip73191@gmai.com>
2025-11-19 05:10:27 +0530

    rendering files is dont using files in the utils folder

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 import http from 'node:http'
 import fs from 'node:fs/promises'
 import path from "node:path"
-
+import { serveStatic } from './utils/serveStatic.js'
 const PORT = 8001
 const __dirname = import.meta.dirname
 //console.log('CWD',process.cwd())
 
 const server = http.createServer(async(req,res) =>{
-   let pathResource = path.join(__dirname,'public','index.html')
-    //const content = fs.readFileSync(pathResource,'utf-8')
-    const content = await fs.readFile(pathResource,'utf-8')
-        res.statusCode = 200
-        res.setHeader('Content-Type','text/html')
-        res.end(content)
-
+   await serveStatic(req,res, __dirname)
+    
 })
 
 server.listen(PORT,()=>console.log('server created'))
---
+++ utils/serveStatic.js
@@ -0,0 +1,22 @@
+import path from 'node:path'
+import fs from 'node:fs/promises'
+import { sendResponse } from './sendResponse.js'
+
+export async function serveStatic(req,res,baseDir)
+{ 
+    const filePath = path.join(baseDir,'public','index.html')
+    try
+    {
+        
+        const content = await fs.readFile(filePath)
+        
+        console.log( typeof content)
+        console.log(res)
+        sendResponse(res,200,'text/html',content)
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+
+}
